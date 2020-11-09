113b6f9482990121056bf5ddfc19c3f5cef6a89f
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-11-09 20:59:50 +0900

    購入済み商品の購入ページ直接アクセス拒否

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -36,7 +36,7 @@ class OrdersController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:item_id])
-    unless user_signed_in?  && current_user.id != @item.user_id
+    unless user_signed_in?  && current_user.id != @item.user_id && @item.order.blank?
       redirect_to root_path
     end
   end
