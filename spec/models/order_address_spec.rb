1fd9e93d5f132cefaa4ab392343c3a6c2a5fc268
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-11-09 23:26:24 +0900

    order_addressモデルの単体テストコード追加

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -13,7 +13,90 @@ RSpec.describe OrderAddress, type: :model do
         end
       end
 
+      context '商品を出品できない場合' do
+        it 'tokenが空では商品購入ができない' do
+          @order_address.token = nil
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Token can't be blank")
+        end
 
+        it 'postal_codeが空では商品購入ができない' do
+          @order_address.postal_code = nil
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+        end
+
+        it 'postal_codeが全角では商品購入ができない' do
+          @order_address.postal_code = '１１１−１１１１'
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Postal code is invalid")
+        end
+
+        it 'postal_codeにハイフンがない場合商品購入ができない' do
+          @order_address.postal_code = '1112222'
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Postal code is invalid")
+        end
+
+        it 'postal_codeが（3桁半角数字 ハイフン 4桁半角数字)の形でない場合商品購入ができない' do
+          @order_address.postal_code = '1112-222'
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Postal code is invalid")
+        end
+
+        it 'prefecture_idが空では商品購入ができない' do
+          @order_address.prefecture_id = nil
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+        end
+
+        it 'prefecture_idが0では商品購入ができない' do
+          @order_address.prefecture_id = 0
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Prefecture must be other than 0")
+        end
+
+        it 'cityが空では商品購入ができない' do
+          @order_address.city = nil
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("City can't be blank")
+        end
+
+        it 'addressが空では商品購入ができない' do
+          @order_address.address = nil
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Address can't be blank")
+        end
+
+        it 'phone_numberが空では商品購入ができない' do
+          @order_address.phone_number = nil
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+        end
+
+        it 'phone_numberが全角では商品購入ができない' do
+          @order_address.phone_number = '０９０１１１１２２２２'
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+        end
+
+        it 'phone_numberにハイフンが含まれる場合商品購入ができない' do
+          @order_address.phone_number = '090-1111-2222'
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+        end
+
+        it 'phone_numberが10桁未満の場合商品購入ができない' do
+          @order_address.phone_number = '090111222'
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+        end
+        it 'phone_numberが12桁以上の場合商品購入ができない' do
+          @order_address.phone_number = '090111122222'
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+        end
+      end
     end
   end
 end
