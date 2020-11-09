f9889af6eb848bf27d0913ef0d3fbb841fae2d74
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-11-09 21:46:26 +0900

    カード情報のエラーメッセージ表示

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,6 +3,7 @@ class OrderAddress
   attr_accessor :postal_code, :prefecture_id, :city, :address, :apartment_name, :phone_number, :token, :item_id, :user_id
 
   with_options presence: true do
+    validates :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :prefecture_id, numericality: { other_than: 0 }
     validates :city
@@ -11,7 +12,6 @@ class OrderAddress
   end
 
   def save
-
     order = Order.create( token: token, item_id: item_id, user_id: user_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, apartment_name: apartment_name, phone_number: phone_number, order_id: order.id)
   end
