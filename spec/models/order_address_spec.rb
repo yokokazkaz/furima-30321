1afcf71445d06c3641051aa13985b25129c09bda
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-11-09 23:02:20 +0900

    商品が購入できる場合のテストコード追加

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @order_address = FactoryBot.build(:order_address)
+    end
+
+    describe '商品を購入する' do
+      context '商品を購入できる場合' do
+        it 'トークン、郵便番号、都道府県、市町村、番地、電話番号があれば商品が購入できる' do
+          expect(@order_address).to be_valid
+        end
+      end
+
+
+    end
+  end
 end
