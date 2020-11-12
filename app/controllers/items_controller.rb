64b3e31d7935d44b80bcb5c2eb8f8e5936ad8dc2
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-11-12 21:55:51 +0900

    条件の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    redirect_to root_path if @item.destroy
+    if current_user.id == @item.user_id
+        @item.destroy
+        redirect_to root_path
+    end
   end
 
   private
