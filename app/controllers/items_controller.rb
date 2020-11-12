938342a6c7ebf14f6e588b7c3f23bbdbb1cc07f4
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-11-12 19:44:37 +0900

    before_action :authenticate_user!の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :item_find, only: [:show, :edit, :update]
-  before_action :authenticate_user!, only: :edit
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     @items = Item.all.order('created_at DESC').includes(:user)
